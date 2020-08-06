fca7819b92f450d5516e1e9af1c476068b63e79e
Youri Cadet <cadetjesusyouri@gmail.com>
2020-08-06 04:15:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,10 @@
-
+def roll_call_dwarves(dwarves)
+  i = 0
+  while i < dwarves.length
+    puts "#{i + 1}. #{dwarves[i]}"
+    i += 1
+  end
+end
  
   # Your code here
   def summon_captain_planet(planeteer_calls)
