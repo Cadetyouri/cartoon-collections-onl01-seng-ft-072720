5e578c4305ffc3f801c611423ccf74228a2dc0f4
Youri Cadet <cadetjesusyouri@gmail.com>
2020-08-06 04:16:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,21 +6,8 @@ def roll_call_dwarves(dwarves)
   end
 end
  
-  # Your code here
-  def summon_captain_planet(planeteer_calls)
+def summon_captain_planet(planeteer_calls)
   planeteer_calls.map do |i|
     i.capitalize + "!"
   end
-
-  # Your code 
-  def long_planeteer_calls(calls_long)
-  calls_long.any? do |i|
-    i.length > 4
-  end
-
-def find_the_cheese(array)
- cheese_types = ["cheddar", "gouda", "camembert"]
-  index = 0
-  array.find do |i|
-    i == cheese_types[index]
-  end
+end
